1d33376814dda6cea7552902c951eb28c68c7a34
Dmitry <dc914337@gmail.com>
2015-07-12 19:23:57 +0300

    shared prefs saves db path and shows summary

--- ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/activities/MainActivity.java
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/activities/MainActivity.java
@@ -1,16 +1,21 @@
 package anon.psd.gui.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import anon.psd.R;
+import anon.psd.models.gui.PrettyPassword;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    int debug_count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,6 +23,17 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(getApplicationContext(), "Created", Toast.LENGTH_SHORT).show();
+        loadBase();
+
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "Resumed " + debug_count++, Toast.LENGTH_SHORT).show();
     }
 
 
@@ -46,15 +62,33 @@ public class MainActivity extends ActionBarActivity
     }
 
 
+    private boolean loadBase()
+    {
+        //check if base path is set
+        SharedPreferences prefs = this.getSharedPreferences(
+                "anon.psd", Context.MODE_PRIVATE);
+        String path = prefs.getString("db_path", null);
+
+        //check if it exists
+        //+load base
+        //-redirect to setting to set path
+
+        //check if user pass set
+        //+load base using pass
+        //-redirect to EnterUserPass page
+
+        //check if loaded
+        //- reset user password and start over
+
+        return true;
+    }
 
 
-
-
-
-
-
-
-
+    public void openItem(PrettyPassword item)
+    {
+        Intent intent = new Intent(this, PassActivity.class);
+        startActivity(intent);
+    }
 
 
     public void openSettings(MenuItem item)
@@ -62,10 +96,6 @@ public class MainActivity extends ActionBarActivity
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
-
-
-
-
 
 
 }
--- ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/preferences/PathPreference.java
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/preferences/PathPreference.java
@@ -6,16 +6,41 @@ import android.util.AttributeSet;
 
 public class PathPreference extends Preference
 {
+    String value;
+
+
     public PathPreference(Context context, AttributeSet attrs)
     {
         super(context, attrs);
     }
 
     @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue)
+    {
+        if (restorePersistedValue) {
+            // Restore existing state
+            setValue(this.getPersistedString((String) defaultValue));
+        } else {
+            // Set default state from the XML attribute
+            setValue((String) defaultValue);
+        }
+    }
+
+    private void setValue(String value)
+    {
+        persistString(value);
+        setSummary(value);
+    }
+
+
+    @Override
     public void onClick()
     {
         //call open file dialog
         //set value to opened file
-        setSummary("/home/sdcard0/PSD/phonePsdBase.psd");
+        value = "/home/sdcard0/PSD/phonePsdBase.psd";
+        setValue(value);
+
+
     }
 }
