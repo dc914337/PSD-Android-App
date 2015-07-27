1a340eba51f429bf9fe182948d7ecdbc1b55e1cc
Dmitry <dc914337@gmail.com>
2015-07-27 10:32:44 +0300

    loading works! wow

--- ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/activities/EnterPassActivity.java
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/activities/EnterPassActivity.java
@@ -1,32 +1,45 @@
 package anon.psd.gui.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import anon.psd.R;
+import anon.psd.storage.PreferencesProvider;
 
 /**
  * Created by Dmitry on 25.07.2015.
  */
 public class EnterPassActivity extends Activity
 {
+
+    TextView txtPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_password);
-
+        txtPass = (TextView) findViewById(R.id.txtPass);
     }
 
 
     public void btnOnClick(View view)
     {
-
+        new PreferencesProvider(this).setUserPass(txtPass.getText().toString());
+        finish();
     }
 
     public void btnSettingsClick(View view)
     {
+        openSettings();
+    }
 
+    public void openSettings()
+    {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
