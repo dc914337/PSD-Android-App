c66a23e6f43f52d7599ab3d692f02a84ed01b9e4
Dmitry <dc914337@gmail.com>
2015-08-03 05:34:15 +0300

    bluetooth interface implementation

---
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/hardware/BluetoothStub.java
@@ -0,0 +1,8 @@
+package anon.psd.hardware;
+
+/**
+ * Created by Dmitry on 03.08.2015.
+ */
+public class BluetoothStub
+{
+}
---
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/hardware/BluetoothWrapper.java
@@ -0,0 +1,9 @@
+package anon.psd.hardware;
+
+/**
+ * Created by Dmitry on 03.08.2015.
+ */
+public class BluetoothWrapper
+{
+
+}
