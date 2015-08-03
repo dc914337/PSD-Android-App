9b6753766905a16f180e8cc075460041435058bf
Dmitry <dc914337@gmail.com>
2015-08-03 05:45:08 +0300

    public bluetooth stub

--- ANDROID PSD/PSD/app/src/main/java/anon/psd/hardware/BluetoothStub.java
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/hardware/BluetoothStub.java
@@ -1,8 +1,74 @@
 package anon.psd.hardware;
 
+import java.util.Random;
+
 /**
  * Created by Dmitry on 03.08.2015.
  */
-public class BluetoothStub
+public class BluetoothStub implements IBluetoothWrapper
 {
+    Random rnd = new Random();
+    boolean connected = false;
+
+    @Override
+    public void enableBluetooth()
+    {
+        randomSleep(2500, 4000);
+    }
+
+    @Override
+    public void disableBluetooth()
+    {
+        randomSleep(1000, 1500);
+    }
+
+    @Override
+    public boolean tryConnectDevice()
+    {
+        randomSleep(1000, 1500);
+        connected = trueRandom(80);
+        return connected;
+    }
+
+    @Override
+    public void disconnectDevice()
+    {
+        randomSleep(1000, 1500);
+    }
+
+    @Override
+    public boolean getConnected()
+    {
+        return connected;
+    }
+
+    @Override
+    public int getSignalStrength()
+    {
+        randomSleep(1000, 1500);
+        if (!connected)
+            return 0;
+        return properRandom(1, 100);
+    }
+
+
+    private void randomSleep(int from, int to)
+    {
+        try {
+            Thread.sleep(properRandom(from, to));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int properRandom(int from, int to)
+    {
+        return from + rnd.nextInt(to - from);
+    }
+
+    private boolean trueRandom(int percentTrue)
+    {
+        int randomNumber = rnd.nextInt(100);
+        return randomNumber < percentTrue;
+    }
 }
--- ANDROID PSD/PSD/app/src/main/java/anon/psd/hardware/BluetoothWrapper.java
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/hardware/BluetoothWrapper.java
@@ -3,7 +3,42 @@ package anon.psd.hardware;
 /**
  * Created by Dmitry on 03.08.2015.
  */
-public class BluetoothWrapper
+public class BluetoothWrapper implements IBluetoothWrapper
 {
 
+    @Override
+    public void enableBluetooth()
+    {
+
+    }
+
+    @Override
+    public void disableBluetooth()
+    {
+
+    }
+
+    @Override
+    public boolean tryConnectDevice()
+    {
+        return false;
+    }
+
+    @Override
+    public void disconnectDevice()
+    {
+
+    }
+
+    @Override
+    public boolean getConnected()
+    {
+        return false;
+    }
+
+    @Override
+    public int getSignalStrength()
+    {
+        return 0;
+    }
 }
