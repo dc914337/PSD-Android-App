0411fa744619ec90b352bf7b5dc76e60a2c7a9e6
Dmitry <dc914337@gmail.com>
2015-11-16 13:51:53 +0300

    roll keys

--- ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/activities/RollbackActivity.java
+++ ANDROID PSD/PSD/app/src/main/java/anon/psd/gui/activities/RollbackActivity.java
@@ -2,19 +2,35 @@ package anon.psd.gui.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import anon.psd.R;
+import anon.psd.background.activity.PsdServiceWorker;
+import anon.psd.gui.exchange.ActivitiesExchange;
 
 /**
  * Created by Dmitry on 13.11.2015.
  */
 public class RollbackActivity extends Activity
 {
+    PsdServiceWorker serviceWorker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rollback);
+        serviceWorker = ActivitiesExchange.getAndRemoveObject("SERVICE_WORKER");
     }
 
+    public void onDoNothingClick(View view)
+    {
+        finish();
+    }
+
+    public void onRollKeysClick(View view)
+    {
+        serviceWorker.rollKeys();
+        finish();
+    }
 }
